16fec4e1f308fcb87e7592cc4fb71e077a508a4b
gjardas <guilherme.fjardim@gmail.com>
2024-11-04 23:10:24 -0300

    Finalizado

--- src/components/Perfil.tsx
+++ src/components/Perfil.tsx
@@ -8,15 +8,18 @@ const Perfil = () => {
 
     return (
         <div className="flex justify-center items-center min-h-screen bg-[#5A85C6]">
-        <div className="bg-white shadow-lg rounded-lg p-6 max-w-sm">
-            <h1 className="text-2xl font-bold text-center mb-4">Perfil</h1>
-            <div className="flex justify-center mb-4">
-               <p>FOTO PERFIL</p>
+            <div className="bg-white shadow-lg rounded-lg p-10 max-w-xl min-w-[300px] min-h-[400px]">
+                <h1 className="text-2xl font-bold text-center mb-4">Perfil</h1>
+                <div className="flex justify-center mb-4">
+                    <p>FOTO PERFIL</p>
+                </div>
+                <p className="text-center text-lg">{cadastro.nome}</p>
             </div>
-            <p className="text-center text-lg">{cadastro.nome}</p>
         </div>
-    </div>
     );
+    
+    
+    
 };
 
 export default Perfil;
